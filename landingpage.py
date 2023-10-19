ebfc3283c12667c1a08d37ca70af452ae5eba80f
doxxedd <dinaridaniel107@gmail.com>
2023-10-19 02:16:24 -0400

    added title and time, moved prompt location

--- landingpage.py
+++ landingpage.py
@@ -1,7 +1,6 @@
 import curses
 import time
 import dashboard
-from curses.textpad import Textbox, rectangle
 
 
 def set_terminal_size(rows, cols):
@@ -9,6 +8,10 @@ def set_terminal_size(rows, cols):
     curses.resizeterm(rows, cols)  # Set the terminal size
     stdscr.refresh()
     return stdscr
+
+
+def get_center_x(prompt):
+    return int((curses.COLS - len(prompt)) / 2)
 
 
 def display_form_element(stdscr, prompt, value, row, col):
@@ -30,32 +33,49 @@ def get_input(stdscr, prompt, row, col):
     return value
 
 
+def show_title(stdscr):
+    title = "Tasker"
+    title_y, title_x = 1, int((curses.COLS - len(title)) / 2)
+    stdscr.addstr(title_y, title_x, title, curses.color_pair(1))
+
+
+def show_time(stdscr):
+    current_time = time.strftime('%H:%M:%S')
+    time_y, time_x = 1, curses.COLS - len(current_time) - 1
+    stdscr.addstr(time_y, time_x, current_time, curses.color_pair(2))
+
+
 def landing_page(stdscr):
     # set_terminal_size(1200, 1000)
     curses.curs_set(1)
     stdscr.clear()
+
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+
+    show_time(stdscr)
+    show_title(stdscr)
     stdscr.refresh()
 
-    login_form = [
-        ("Username: ", ""),
-        ("Password: ", "")
-    ]
+    login_form = [["Username: ", ""], ["Password: ", ""]]
 
     current_row = 5
     for label, value in login_form:
-        display_form_element(stdscr, label, value, current_row, 10)
+        display_form_element(stdscr, label, value, current_row, 2)
         current_row += 2
 
     # Get user input for username and password
-    username = get_input(stdscr, "", 5, 20)
-    password = get_input(stdscr, "", 7, 20)
+    username = get_input(stdscr, "", 5, len(login_form[0][0]) + 2)
+    password = get_input(stdscr, "", 7, len(login_form[1][0]) + 2)
 
     # Check for login (demo always allows access)
     if username == "test" and password == "test":
-        stdscr.addstr(9, 10, "Login successful!", curses.A_BOLD)
+        stdscr.addstr(9, get_center_x("Login successful!"), "Login successful!", curses.A_BOLD)
         dashboard.dashboard(stdscr)
     else:
-        stdscr.addstr(9, 10, "Login failed. Please try again.", curses.A_BOLD)
+        curses.curs_set(0)
+        stdscr.addstr(9, get_center_x("Login failed."), "Login failed.", curses.A_BOLD)
 
     stdscr.refresh()
     stdscr.getch()
