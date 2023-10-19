e7f22bf0d830d03b2a91aebbc6bff6c8f7a46708
doxxedd <dinaridaniel107@gmail.com>
2023-10-18 23:11:34 -0400

    login page test

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,6 @@
 import curses
 import time
-import getpass
+from curses.textpad import Textbox, rectangle
 
 
 def set_terminal_size(rows, cols):
@@ -10,62 +10,50 @@ def set_terminal_size(rows, cols):
     return stdscr
 
 
+def display_form_element(stdscr, prompt, value, row, col):
+    stdscr.addstr(row, col, prompt)
+    stdscr.addstr(row, col + len(prompt), value)
+
+
+def get_input(stdscr, prompt, row, col):
+    stdscr.addstr(row, col, prompt)
+    stdscr.refresh()
+    input_window = curses.newwin(1, 30, row, col + len(prompt))
+    input_window.keypad(True)
+
+    curses.echo()
+    input_window.refresh()
+    value = input_window.getstr(0, 0).decode('utf-8')
+    curses.noecho()
+
+    return value
+
+
 def landing_page(stdscr):
     # set_terminal_size(1200, 1000)
+    curses.curs_set(1)
     stdscr.clear()
-    curses.curs_set(0)
+    stdscr.refresh()
 
-    curses.start_color()
-    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    login_form = [
+        ("Username: ", ""),
+        ("Password: ", "")
+    ]
 
-    title = "Tasker"
-    title_y, title_x = 2, int((curses.COLS - len(title)) / 2)
+    current_row = 5
+    for label, value in login_form:
+        display_form_element(stdscr, label, value, current_row, 10)
+        current_row += 2
 
-    current_time = time.strftime('%H:%M:%S')
-    time_y, time_x = 1, curses.COLS - len(current_time) - 1
+    # Get user input for username and password
+    username = get_input(stdscr, "", 5, 20)
+    password = get_input(stdscr, "", 7, 20)
 
+    # Check for login (demo always allows access)
+    if username == "test" and password == "test":
+        stdscr.addstr(9, 10, "Login successful!", curses.A_BOLD)
+    else:
+        stdscr.addstr(9, 10, "Login failed. Please try again.", curses.A_BOLD)
 
-    username = ""
-    password = ""
-    username_label = "Username: "
-    password_label = "Password: "
-
-    active_input = username
-
-    while True:
-        stdscr.clear()
-
-        # Display title
-        stdscr.addstr(title_y, title_x, title, curses.color_pair(1))
-
-        # Draw username input box
-        stdscr.addstr(title_y + 2, title_x, username_label)
-        stdscr.addstr(title_y + 2, title_x + len(username_label), username, curses.color_pair(2))
-
-        # Draw password input box (hide the password with asterisks)
-        stdscr.addstr(title_y + 3, title_x, password_label)
-        stdscr.addstr(title_y + 3, title_x + len(password_label), '*' * len(password), curses.color_pair(2))
-
-        if active_input == username:
-            stdscr.move(title_y + 2, title_x + len(username) + len(username_label))
-        else:
-            stdscr.move(title_y + 3, title_x + len(password) + len(password_label))
-
-        stdscr.refresh()
-
-        # Get user input
-        key = stdscr.getch()
-
-        if key == curses.KEY_ENTER:  # Enter key
-            active_input = password  # Change the active input to password
-        elif key == curses.KEY_BACKSPACE:
-            if active_input == username:
-                username = username[:-1]
-            else:
-                password = password[:-1]
-        else:
-            if active_input == username:
-                username += chr(key)
-            else:
-                password += chr(key)
+    stdscr.refresh()
+    stdscr.getch()
