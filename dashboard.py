90477982741feab87d89e97bff650086a89dac67
Arjun <20ad47@queensu.ca>
2023-10-20 13:07:04 -0400

    Added comments

--- dashboard.py
+++ dashboard.py
@@ -1,14 +1,33 @@
+"""
+Code for dashboard where user is prompted to choose from various action options
+"""
+
 import curses
 import datetime
 from curses.textpad import Textbox, rectangle
 
 
 def display_form_element(stdscr, prompt, value, row, col):
+    """
+    Function to displays element that needs user input
+    :param prompt: Text to display as prompt for input
+    :param value: User input
+    :param row: Row coordinate for where to display element on screen
+    :param col: Column coordinate for where to display element on screen
+    :return: None
+    """
     stdscr.addstr(row, col, prompt)
     stdscr.addstr(row, col + len(prompt), value)
 
 
 def get_input(stdscr, prompt, row, col):
+    """
+    Function to allow user to type input
+    :param prompt: Text to display as prompt for input
+    :param row: Row coordinate for where to display element on screen
+    :param col: Column coordinate for where to display element on screen
+    :return: Value that was inputted
+    """
     stdscr.addstr(row, col, prompt)
     stdscr.refresh()
     input_window = curses.newwin(1, 30, row, col + len(prompt))
@@ -23,13 +42,20 @@ def get_input(stdscr, prompt, row, col):
 
 
 def create_field_page(stdscr, option, field1, field2, field3):
-    
+    """
+    General function to input fiels for tasks or projects
+    :param option: Option selected by user (specific action for project or task)
+    :param field1: Field prompt for input by user
+    :param field2: Field prompt for input by user
+    :param field3: Field prompt for input by user
+    :return: None
+    """
     curses.curs_set(1)
     stdscr.clear()
     stdscr.refresh()
     # Create Task
     stdscr.addstr(5, 30, f"You chose '{option}'")
-    taskoptions =  ["Create Task", "Modify Task", "Remove Task"]
+    taskoptions = ["Create Task", "Modify Task", "Remove Task"]
     projectoptions = ["Create Project", "Modify Project", "Remove Project"]
     detail_form = [
         (f"{field1}: ", ""),
@@ -42,7 +68,7 @@ def create_field_page(stdscr, option, field1, field2, field3):
         display_form_element(stdscr, label, value, current_row, 25)
         current_row += 2
     # Get user input for username and password
- 
+
     if option in taskoptions:
         field1 = get_input(stdscr, "", 10, 35)
         field2 = get_input(stdscr, "", 12, 35)
@@ -51,9 +77,6 @@ def create_field_page(stdscr, option, field1, field2, field3):
         field1 = get_input(stdscr, "", 10, 35)
         field2 = get_input(stdscr, "", 12, 40)
         field3 = get_input(stdscr, "", 14, 35)
-     
-
-
 
     if field1 == "test" and field2 == "test" and field3 == "test":
         stdscr.addstr(18, 35, "success!", curses.A_BOLD)
@@ -62,10 +85,13 @@ def create_field_page(stdscr, option, field1, field2, field3):
 
     stdscr.refresh()
     stdscr.getch()
-    
 
 
 def dashboard(stdscr):
+    """
+    Function to display dashboard for the user
+    :return: None
+    """
     stdscr.clear()
     # Set up colors
     curses.start_color()
@@ -74,7 +100,6 @@ def dashboard(stdscr):
 
     options = ["Create Task", "Modify Task", "Remove Task", "Create Project", "Modify Project", "Remove Project"]
 
-    
     selected_row = 0
 
     curses.curs_set(0)  # Hide the cursor
@@ -124,4 +149,4 @@ def dashboard(stdscr):
                 # Remove Project
                 stdscr.addstr(5, 30, "You chose 'Remove Project'")
             stdscr.refresh()
-            stdscr.getch() 
+            stdscr.getch()
--- landingpage.py
+++ landingpage.py
@@ -1,9 +1,18 @@
+"""
+User landing page where user is prompted for login details
+"""
 import curses
 import time
 import dashboard
 
 
 def set_terminal_size(rows, cols):
+    """
+    Function to set size of terminal displayed
+    :param rows: Row size of terminal
+    :param cols: Column size of terminal
+    :return: stdscr
+    """
     stdscr = curses.initscr()
     curses.resizeterm(rows, cols)  # Set the terminal size
     stdscr.refresh()
@@ -11,15 +20,35 @@ def set_terminal_size(rows, cols):
 
 
 def get_center_x(prompt):
+    """
+    Function to find center column value for terminal size based on the length of the prompt to be displayed
+    :param prompt: Text prompt to be displayed
+    :return: Coordinate of center x value
+    """
     return int((curses.COLS - len(prompt)) / 2)
 
 
 def display_form_element(stdscr, prompt, value, row, col):
+    """
+    Function to displays element that needs user input
+    :param prompt: Text to display as prompt for input
+    :param value: User input
+    :param row: Row coordinate for where to display element on screen
+    :param col: Column coordinate for where to display element on screen
+    :return: None
+    """
     stdscr.addstr(row, col, prompt)
     stdscr.addstr(row, col + len(prompt), value)
 
 
 def get_input(stdscr, prompt, row, col):
+    """
+    Function to allow user to type input
+    :param prompt: Text to display as prompt for input
+    :param row: Row coordinate for where to display element on screen
+    :param col: Column coordinate for where to display element on screen
+    :return: Value that was inputted
+    """
     stdscr.addstr(row, col, prompt)
     stdscr.refresh()
     input_window = curses.newwin(1, 30, row, col + len(prompt))
@@ -34,32 +63,54 @@ def get_input(stdscr, prompt, row, col):
 
 
 def show_title(stdscr):
+    """
+    Function to display application title on landing page
+    :return: None
+    """
     title = "Tasker"
+    # display in the center of the screen
     title_y, title_x = 1, int((curses.COLS - len(title)) / 2)
     stdscr.addstr(title_y, title_x, title, curses.color_pair(1))
 
 
 def show_time(stdscr):
+    """
+    Function to display time on landing page
+    :return: None
+    """
+    # get current time
     current_time = time.strftime('%H:%M:%S')
+    # display on top right of screen
     time_y, time_x = 1, curses.COLS - len(current_time) - 1
     stdscr.addstr(time_y, time_x, current_time, curses.color_pair(2))
 
 
 def landing_page(stdscr):
+    """
+    Function to display landing page
+    :return: None
+    :rtype:
+    """
     # set_terminal_size(1200, 1000)
     curses.curs_set(1)
     stdscr.clear()
 
+    # initialize colors
     curses.start_color()
     curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
 
+    # display the time
     show_time(stdscr)
+
+    # display title
     show_title(stdscr)
     stdscr.refresh()
 
+    # list of prompts
     login_form = [["Username: ", ""], ["Password: ", ""]]
 
+    # display prompts and input fields
     current_row = 5
     for label, value in login_form:
         display_form_element(stdscr, label, value, current_row, 2)
