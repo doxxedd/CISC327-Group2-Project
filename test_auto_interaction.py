00a5903f8254918071d61662bcc52e25973806b7
doxxedd <dinaridaniel107@gmail.com>
2023-11-13 21:32:20 -0500

    added dict for pytest

--- test_auto_interaction.py
+++ test_auto_interaction.py
@@ -3,6 +3,8 @@ import threading
 import time
 import landingpage
 import pyautogui as p
+import dashboard
+dict = {}
 
 
 def create_instance(stdscr):
@@ -18,46 +20,61 @@ def worker():
     create_task("title", "details")
     create_project("title", "details")
     modify_task("new title", "new details")
-    modify_project("new project title", "new project detailss")
+    modify_project("new project title")
 
 
 def login():
     # as test:test
     for _ in range(2):
         p.typewrite("test\n")
+    dict["login"] = landingpage.testresult
 
 
 def create_task(title, details):
+    """creates task with given title and details. today's deadline
+    """
     p.typewrite(f"{title}\n{details}\n")
     # Deadline is always today in this test
     p.press('enter', presses=2)
+    dict["create_task"] = dashboard.test
 
 
 def create_project(title, details):
+    """creates project with given title and details. today's deadline
+    """
     p.press("down", presses=3)
     p.press("enter")
     p.typewrite(f"{title}\n{details}\n")
     p.press("enter")
+    dict["create_project"] = dashboard.test
 
 
 def modify_task(title, details):
+    """modifies task title and details
+    """
     p.press("up", presses=2)
     p.press("enter")
     p.press("1")
     p.press("enter")
     p.typewrite(f"{title}\n{details}\n\n")
+    dict["modify_task"] = dashboard.test
 
 
-def modify_project(title, details):
+def modify_project(title):
+    """changes title and assigns task to project
+    """
     p.press("down", presses=3)
     p.press("enter")
     p.press("1")
     p.press("enter")
     p.press("1")
     p.press("enter")
-    p.typewrite(f"{title}\n")
+    p.typewrite(f"{title}\n\n1\n")
 
+
+def main():
+    curses.wrapper(create_instance)
 
 
 if __name__ == "__main__":
-    curses.wrapper(create_instance)
+    main()
