66611f15b220aff6b594a9b070b96b0d8eefad4a
alexdobro4 <alex.s.dobrovolskyy@gmail.com>
2023-11-13 19:23:18 -0500

    bugs

--- test_auto_interaction.py
+++ test_auto_interaction.py
@@ -4,34 +4,35 @@ import time
 import keyboard
 import landingpage
 
-def curses_main(stdscr):
-    stdscr.clear()
-    stdscr.addstr(0, 0, "Press 'q' to quit")
 
-    while True:
-        key = stdscr.getch()
-        if key == ord('q'):
-            break
 
-def keyboard_listener(stdscr):
-    # time.sleep(2)
+
+def keyboard_listener():
+    time.sleep(2)
     keyboard.write('test')
     # time.sleep(2)
     keyboard.press_and_release("enter")
     keyboard.write('test')
-    # time.sleep(2)
+    time.sleep(2)
+    keyboard.press_and_release("enter")
+    keyboard.press_and_release("enter")
+    keyboard.write("test")
+    keyboard.press_and_release("enter")
+    keyboard.write("test")
+    keyboard.press_and_release("enter")
     keyboard.press_and_release("enter")
 
 
 def main(stdscr):
-    curses_thread = threading.Thread(target=landingpage.landing_page, args=(stdscr,))
-    keyboard_thread = threading.Thread(target=keyboard_listener, args=(stdscr,))
-
-    curses_thread.start()
+    # curses_thread = threading.Thread(landingpage.landing_page(stdscr))
+    keyboard_thread = threading.Thread(target=keyboard_listener)
+    # curses_thread.start()
     keyboard_thread.start()
 
-    curses_thread.join()
+    # curses_thread.join()
     keyboard_thread.join()
+    landingpage.landing_page(stdscr)
+    
 
 if __name__ == "__main__":
 
