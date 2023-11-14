e51cc523a3d7d58b2bd38212612e732cf27c2ec9
doxxedd <dinaridaniel107@gmail.com>
2023-11-13 20:47:01 -0500

    added create_project

--- test_auto_interaction.py
+++ test_auto_interaction.py
@@ -13,21 +13,31 @@ def create_instance(stdscr):
 
 
 def worker():
-    time.sleep(1)
+    time.sleep(0.5)
     login()
     create_task("title", "details")
+    create_project("title", "details")
 
 
 def login():
+    # as test:test
     for _ in range(2):
         p.typewrite("test\n")
 
 
 def create_task(title, details):
-    p.typewrite(f"{title}\n")
-    p.typewrite(f"{details}\n")
+    p.typewrite(f"{title}\n{details}\n")
     # Deadline is always today in this test
     p.press('enter', presses=2)
+
+def create_project(title, details):
+    p.press("down", presses=3)
+    p.press("enter")
+    p.typewrite(f"{title}\n{details}\n")
+    p.press("enter")
+
+def modify_task(title, details):
+    pass
 
 if __name__ == "__main__":
     curses.wrapper(create_instance)
