a9bd5fa415b9c99e056ea7e3f0a9d1cd091f391a
doxxedd <dinaridaniel107@gmail.com>
2023-11-13 23:19:50 -0500

    added BB 1

--- test_testcases.py
+++ test_testcases.py
@@ -15,83 +15,70 @@ cursor = conn.cursor()
 # current_user = User()  # Initialize current_user as an instance of the User class
 
 def test_login():
-
     result = shared.dict["login"]
     # Check the output to verify the expected message
-   
+
     assert result is True
+
 
 def test_wrong_login():
-    
-
-    result = landingpage.validate_user("unkown", "test")
+    unknown_list = ["unkown", "testtttt", "1", "1B", "B1", "()", "+-", "#"]
+    for i in unknown_list:
+        result = landingpage.validate_user(i, i)
     # Check the output to verify the expected message
-   
-    assert result is True
+    assert result is False
+
 
 def test_create_task():
-    
-
     result = shared.dict["create_task"]
     # Check the output to verify the expected message
-   
     assert result == "Task Created Succesfully"
 
-def test_create_project():
 
+def test_create_project():
     result = shared.dict["create_project"]
     # Check the output to verify the expected message
-   
     assert result == "Project Created Succesfully"
 
 
 def test_modify_project():
-
     result = shared.dict["create_project"]
     # Check the output to verify the expected message
-   
     assert result == "Project Created Succesfully"
 
-def test_modify_task():
 
+def test_modify_task():
     result = shared.dict["modify_task"]
     # Check the output to verify the expected message
-   
     assert result == "Task Modified Successfully"
 
 def test_modify_project():
-
     result = shared.dict["modify_project"]
     # Check the output to verify the expected message
-   
     assert result == "Project Title Modified successfully, Task Added to Project Successfully"
 
-def test_remove_task():
 
+def test_remove_task():
     result = shared.dict["remove_task"]
     # Check the output to verify the expected message
-   
     assert result == "Task Deleted Successfully"
 
-def test_remove_project():
 
+def test_remove_project():
     result = shared.dict["remove_project"]
     # Check the output to verify the expected message
-   
     assert result == "Project Deleted Successfully"
 
-def test_view_task():
 
+def test_view_task():
     result = shared.dict["view_task"]
     # Check the output to verify the expected message
-   
     assert result == "Tasks Viewed Successfully"
 
-def test_view_project():
 
+def test_view_project():
     result = shared.dict["view_project"]
     # Check the output to verify the expected message
-   
     assert result == "Project Viewed Successfully"
 
 
